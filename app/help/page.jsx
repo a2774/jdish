f72b2b3d469f937ef0fb85c09b5d3443e1ef48cc
a2774 <akumar64587@gmail.com>
2025-12-29 20:34:49 +0530

    make home page

--- app/help/page.jsx
+++ app/help/page.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
     <div>
-      <h1>help</h1>
+      <h1>help about</h1>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
