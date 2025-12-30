e804ec07a371e88de30ee58ca1c0f86454cc19f0
a2774 <akumar64587@gmail.com>
2025-12-30 22:25:53 +0530

    fixed buggs

--- app/page.js
+++ app/page.js
@@ -8,16 +8,17 @@ function Page() {
   return (
     <div>
       <Navbar />
+
       <div className="sticky top-20 px-4 py-4 w-full">
-        <div className="flex flex-col md:flex-row ">
+        <div className="flex flex-col md:flex-row">
           <aside className="hidden lg:block lg:col-span-3 xl:col-span-2">
             <div className="sticky top-18">
               <Sidebar />
             </div>
           </aside>
 
-          <section className="col-span-12 lg:col-span-6 xl:col-span-7 ">
-            <div className="mt-0 w-174 flex justify-end items-center  ml-9">
+          <section className="col-span-12 lg:col-span-6 xl:col-span-7 w-full">
+            <div className="mt-0 w-full sm:w-full md:w-full lg:w-174 flex justify-center lg:justify-end items-center ml-0 lg:ml-9">
               <Home />
             </div>
           </section>
